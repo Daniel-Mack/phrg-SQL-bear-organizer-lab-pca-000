f136c108e44df8892dabba457d20b687a64aeb98
Daniel-Mack <daniel.mack@powerhrg.com>
2018-06-11 20:48:43 -0400

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "black", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "M", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "F", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 18, "M", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "F", "black", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "M", "black", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 13, "F", "black", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 11, "M", "black", "happy", "true");
